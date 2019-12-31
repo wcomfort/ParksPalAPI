08f312ab8e98c29c59b746c6f6bf7f0897b8cb79
Will <comfortwb21@gmail.com>
2019-12-31 16:11:46 -0500

    fix login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::API
     def login
         user = User.find_by(username: params[:username])
         user.authenticate(params[:password])
+        if user.authenticate(params[:password]) 
         render json: user.to_json(to_serialized_json)
+        else
+          render json: {message: "Invalid Login"}
+        end
     end
 
     def to_serialized_json
